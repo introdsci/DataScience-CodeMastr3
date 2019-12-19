8720567d2c1f111897e34cc5d425990fe6b3586b
Cody Evans <1123code@gmail.com>
2019-12-19 12:50:12 -0800

    Adding initial part3

---
+++ part2.r
@@ -0,0 +1,7 @@
+## -----------------------------------------------------------------------------
+
+suppressMessages(library("knitr"))
+invisible(purl("part1.Rmd", output = "part1.r"))
+invisible(source("part1.r"))
+
+
